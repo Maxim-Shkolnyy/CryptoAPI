8877edd81303bdb8c3eb23ffb5fb3b514374abf2
Shkolnyi Maxim <@foxstudent102139>
2024-05-23 09:14:43 +0300

    1

---
+++ CryptoAPI/Models/CryptoCurrency.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CryptoAPI.Models
+{
+    public class CryptoCurrency
+    {
+        public int Id { get; set; }
+        public string Symbol { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public DateTime LastUpdated { get; set; }
+    }
+
+}
